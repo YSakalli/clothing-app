3287f568fb4c93d169a00b3ddab88a37ababd61a
YSakalli <ysakallidev@gmail.com>
2024-01-02 21:24:36 +0300

    new page

--- js/app.js
+++ js/app.js
@@ -2,8 +2,15 @@
 
 const menuicon = document.querySelector('.menuicon');
 const menu = document.querySelector('.menu');
+
 const close = document.querySelector('.close');
 const closemenu = document.querySelector('.closemenu');
+
+const piece = document.querySelector('.piece');
+
+const filtericon = document.querySelector('.filtericon');
+const filter = document.querySelector('.filter');
+const darkness = document.querySelector('.darkness');
 
 menuicon.addEventListener('click', function () {
     menu.classList.add('active');
@@ -15,4 +22,15 @@ close.addEventListener('click', function () {
 
 closemenu.addEventListener('click', function () {
     menu.classList.remove('active');
+});
+
+piece.addEventListener('click', function () {
+    piece.classList.toggle('close');
+});
+
+filtericon.addEventListener('click', function () {
+    filter.classList.add('active');
+});
+darkness.addEventListener('click', function () {
+    filter.classList.remove('active');
 });
