2f0273b21d61554979599d7a494a6083cdb0d18e
YSakalli <ysakallidev@gmail.com>
2024-01-04 20:39:38 +0300

    feat

--- js/app.js
+++ js/app.js
@@ -12,6 +12,14 @@ const filtericon = document.querySelector('.filtericon');
 const filter = document.querySelector('.filter');
 const darkness = document.querySelector('.darkness');
 
+const products = document.querySelectorAll('.product');
+
+const closefilter = document.querySelector('.closefilter');
+
+const topInfo = document.querySelector('.info');
+const closetopinfo = document.querySelector('.closetopinfo');
+
+
 menuicon.addEventListener('click', function () {
     menu.classList.add('active');
 
@@ -26,6 +34,10 @@ closemenu.addEventListener('click', function () {
 
 piece.addEventListener('click', function () {
     piece.classList.toggle('close');
+
+    products.forEach(function (product) {
+        product.classList.toggle('size');
+    })
 });
 
 filtericon.addEventListener('click', function () {
@@ -33,4 +45,12 @@ filtericon.addEventListener('click', function () {
 });
 darkness.addEventListener('click', function () {
     filter.classList.remove('active');
+});
+
+
+closefilter.addEventListener('click', function () {
+    filter.classList.remove('active');
+});
+closetopinfo.addEventListener('click', function () {
+    topInfo.style.display = 'none';
 });
